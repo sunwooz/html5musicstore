5615c62f0ff76531bbefaa60508d1326c4b16fd3
Daniel Yang <yangsunwoo@gmail.com>
2013-01-24 19:05:15 -0500

    config.paperclip with s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,6 @@
 Musicstore::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
-
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -73,4 +64,14 @@ Musicstore::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_protocol => 'http',
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
